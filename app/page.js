73c7fc259f92a009b0df26ecaa05cb6aaed2513d
sachinshaw27 <165559429+sachinshaw27@users.noreply.github.com>
2024-07-16 21:57:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h2>Subscribe to Guruji</h2>
-    <Button>Subscribe</Button>
+   <h2> Go to dashboard for giving <span style="color: blue;">mock interview</span></h2>
+    <Button><a href="https://ai-interview-mocker-app-three.vercel.app/dashboard">dashboard</a></Button>
    </div>
   );
 }
