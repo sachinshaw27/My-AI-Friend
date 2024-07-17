ba978831415a7ea6f149cc4711c6b159b6b71aba
sachinshaw27 <165559429+sachinshaw27@users.noreply.github.com>
2024-07-17 12:35:19 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   return (
    <div>
    <h2> Go to dashboard for giving <span style="color: blue;">mock interview</span></h2>
-    <Button onClick={()=> navigate("./dashboard")}>Dashboard</Button>
+    <Button>Hello</Button>
    </div>
   );
 }
